eb0fe4c0b02c49b6dbfb24e7b7a9d3ff96781e0c
im-beltagy <imbeltagy@gmail.com>
2025-05-22 10:08:06 +0300

    chor(otp): add reset password email

--- stage/types/otp.ts
+++ stage/types/otp.ts
@@ -1,6 +1,10 @@
 export const enum OTPPurpose {
   Register = "register",
+  ResetPassword = "reset_password",
 }
-export const allOtpPurposes: OTPPurpose[] = [OTPPurpose.Register];
+export const allOtpPurposes: OTPPurpose[] = [
+  OTPPurpose.Register,
+  OTPPurpose.ResetPassword,
+];
 
 export const OTP_EXPIRE_TIME = 10 * 60 * 1000;
