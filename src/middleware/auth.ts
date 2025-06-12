dc3ca1f12a3e44d708b8b4a512181daf3f757454
im-beltagy <imbeltagy@gmail.com>
2025-06-13 00:42:21 +0300

    feat(auth): enable authenticating using userId as token for development only

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import Unauthorized from "../errors/unauthorized";
-import { verifyUserToken } from "../utils/jwt";
+import { verifyToken, verifyUserToken } from "../utils/jwt";
+import User from "../models/user.model";
 
 export const authMiddleware = async (
   req: Request,
@@ -9,6 +10,17 @@ export const authMiddleware = async (
 ) => {
   const token = req.headers["authorization"]?.split("Bearer ")[1];
   if (!token) throw new Unauthorized();
+
+  // IN DEVELOPMENT, pass user neither using valid token or userId as token
+  const unknownPayload = await verifyToken(token);
+  const isTokenInvalid = !unknownPayload;
+  if (isTokenInvalid && process.env.NODE_ENV === "development") {
+    const user = await User.findById(token);
+    if (!user) throw new Unauthorized();
+    req.currentUser = { sub: user.id, email: user.email, role: user.role };
+    next();
+    return;
+  }
 
   const payload = await verifyUserToken(token);
 
